68ac0c6ec8694e5b6d0069515995a68f9667eba2
dan <kaqilon@aliyun.com>
2020-07-21 23:58:50 +0800

    Paint set Join Miter ColorFilter

--- app/src/main/java/com/dan/learn/lab2/widget/view/PaintView.java
+++ app/src/main/java/com/dan/learn/lab2/widget/view/PaintView.java
@@ -4,7 +4,9 @@ import android.content.Context;
 import android.graphics.BlendMode;
 import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.ColorFilter;
 import android.graphics.ColorMatrixColorFilter;
+import android.graphics.LightingColorFilter;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Typeface;
@@ -36,6 +38,7 @@ public class PaintView extends View {
     private Path linePath2;
     private Path linePath3;
     private Path linePath4;
+    private LightingColorFilter lightingColorFilter;
 
     public PaintView(Context context) {
         super(context);
@@ -95,15 +98,10 @@ public class PaintView extends View {
         paint.setARGB(color, color, color, color);
 
         ColorMatrixColorFilter filter = new ColorMatrixColorFilter(new float[]{0.5f, 0.35f});
+
         paint.setColorFilter(filter);
     }
 
-    public void setPaintStroke() {
-        paint.setStrokeWidth(20);
-        paint.setStrokeCap(Paint.Cap.BUTT);
-//        paint.setStrokeJoin(Paint.Join.BEVEL);
-//        paint.setStrokeMiter(1);
-    }
 
     public void setPaintOther() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
@@ -139,6 +137,7 @@ public class PaintView extends View {
     protected void dispatchDraw(Canvas canvas) {
         String text = "绘制 dispatchDraw 函数 super 前";
         String text2 = "绘制 dispatchDraw 函数 super 后";
+        paint.setColorFilter(null);
         paint.setStyle(Paint.Style.FILL);
         paint.setTextSize(20);
         int textW = (int) paint.measureText(text);
@@ -154,6 +153,7 @@ public class PaintView extends View {
     @Override
     public void onDrawForeground(Canvas canvas) {
         super.onDrawForeground(canvas);
+        paint.setColorFilter(null);
         paint.setStyle(Paint.Style.FILL);
         paint.setTextSize(20);
         String text = "Foreground 绘制";
@@ -166,12 +166,27 @@ public class PaintView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        paint.setColorFilter(null);
         paint.setColor(Color.RED);
 //        paint.setStyle(Paint.Style.FILL);
         canvas.drawCircle(centerX, centerY - 200, 100, paint);
         drawStrokeLine(canvas);
+
+        drawColorFilterMatrix(canvas);
     }
 
+    private void drawColorFilterMatrix(Canvas canvas) {
+        setLightingFilter();
+        paint.setColor(Color.rgb(30,60,120));
+        canvas.drawRect(centerX >> 3, 500, centerX - 200, 600, paint);
+    }
+
+    private void setLightingFilter() {
+        if (lightingColorFilter == null) {
+            lightingColorFilter = new LightingColorFilter(0xFFFF0FFF, 0x000000FF);
+            paint.setColorFilter(lightingColorFilter);
+        }
+    }
 
     private void drawStrokeLine(Canvas canvas) {
         paint.setStrokeWidth(20);
@@ -189,7 +204,6 @@ public class PaintView extends View {
         canvas.drawText(text1, centerX - 80, 100, paint);
         canvas.drawText(text2, centerX - 80, 150, paint);
         canvas.drawText(text3, centerX - 80, 200, paint);
-
 
         drawJoinStrokeLine(canvas);
     }
@@ -215,7 +229,7 @@ public class PaintView extends View {
         canvas.drawText("Join null", centerX + 100, 390, paint);
         canvas.drawText("Join Round 圆角", centerX >> 2, 260, paint);
         canvas.drawText("Join BEVEL 直线", centerX + 100, 260, paint);
-        canvas.drawText("Join MITER 锐角", (centerX >> 2)+ 100, 380, paint);
+        canvas.drawText("Join MITER 锐角", (centerX >> 2) + 100, 380, paint);
     }
 
     private void initLinePath(int pathIndex) {
@@ -227,6 +241,8 @@ public class PaintView extends View {
         }
 
         if (pathIndex == 0) {
+            paint.setStrokeMiter(1.5f);
+            paint.setStrokeJoin(Paint.Join.MITER);
             linePath4.moveTo(centerX, 350);
             linePath4.lineTo(centerX + 300, 350);
             linePath4.lineTo(centerX + 300, 500);
@@ -249,11 +265,14 @@ public class PaintView extends View {
             linePath2.close();
         }
 
+
         if (pathIndex == 3) {
+            paint.setStrokeMiter(60); //配合 paint.setStrokeJoin()使用，当连接角度为比较尖锐的锐角时，控制斜线的连接行为
             paint.setStrokeJoin(Paint.Join.MITER);
             linePath3.moveTo(centerX >> 2, 350);
             linePath3.lineTo(centerX - 100, 350);
             linePath3.lineTo(centerX - 100, 500);
+//            linePath3.lineTo(centerX >> 2,350);
             linePath3.close();
         }
 
