843dd4e88b7c11263ac0a93bc18264270b52143c
kaqilon <kaqilon@aliyun.com>
2020-08-03 18:48:28 +0800

    matrix color filter

--- app/src/main/java/com/dan/learn/lab2/ui/fragment/ViewDrawFragment.java
+++ app/src/main/java/com/dan/learn/lab2/ui/fragment/ViewDrawFragment.java
@@ -1,10 +1,16 @@
 package com.dan.learn.lab2.ui.fragment;
 
+import android.content.res.Resources;
+import android.graphics.Color;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.LightingColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.SeekBar;
 import android.widget.Switch;
@@ -20,10 +26,25 @@ import com.dan.learn.lab2.widget.view.PaintColorView;
 /**
  * 绘制
  */
-public class ViewDrawFragment extends BaseFragment {
+public class ViewDrawFragment extends BaseFragment implements SeekBar.OnSeekBarChangeListener {
 
     private PaintColorView pcv_color_view;
+    private PaintColorView pcv_color_view_2;
     private Switch switch_light_button;
+    private Switch switch_porter_duff_button;
+    private Button bt_change_mode;
+    private SeekBar seek_red;
+    private SeekBar seek_green;
+    private SeekBar seek_blue;
+    private SeekBar seek_alpha;
+    private PaintColorView pcv_color_view_3;
+
+    private int colorRed;
+    private int colorGreen;
+    private int colorBlue;
+    private int colorAlpha;
+    private int offset = 100;
+    private float[] matrixColorArray;
 
     public static ViewDrawFragment getInstance(String title, @LayoutRes int layoutId) {
         return new ViewDrawFragment(title, layoutId);
@@ -35,6 +56,7 @@ public class ViewDrawFragment extends BaseFragment {
     }
 
     private int mLayoutRes;
+    private int mCurrentPorterColorMode;
 
     @Override
     protected int getContentLayout() {
@@ -46,23 +68,121 @@ public class ViewDrawFragment extends BaseFragment {
         super.onViewCreated(view, savedInstanceState);
         switch_light_button = view.findViewById(R.id.switch_light_button);
         pcv_color_view = view.findViewById(R.id.pcv_color_view);
+        pcv_color_view_2 = view.findViewById(R.id.pcv_color_view_2);
+        switch_porter_duff_button = view.findViewById(R.id.switch_porter_duff_button);
+        bt_change_mode = view.findViewById(R.id.bt_change_mode);
+
+        seek_red = view.findViewById(R.id.seek_red);
+        seek_green = view.findViewById(R.id.seek_green);
+        seek_blue = view.findViewById(R.id.seek_blue);
+        seek_alpha = view.findViewById(R.id.seek_alpha);
+        pcv_color_view_3 = view.findViewById(R.id.pcv_color_view_3);
+
         initSeekBar();
+        initPorterDuffColorFilter();
+        initMatrixColorFilter();
     }
 
     private void initSeekBar() {
         if (switch_light_button != null && pcv_color_view != null) {
-            switch_light_button.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-                @Override
-                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                    if (isChecked) {
-                        LightingColorFilter colorFilter = new LightingColorFilter(0xFFFFFF, 0x0000FF);
-                        pcv_color_view.setLightColorFilter(colorFilter);
-                    } else {
-                        pcv_color_view.setLightColorFilter(null);
-                    }
-
+            switch_light_button.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                if (isChecked) {
+                    LightingColorFilter colorFilter = new LightingColorFilter(0xFFFFFF, 0x0000FF);
+                    pcv_color_view.setLightColorFilter(colorFilter);
+                } else {
+                    pcv_color_view.setLightColorFilter(null);
                 }
+
             });
         }
+    }
+
+    private void initPorterDuffColorFilter() {
+        if (switch_porter_duff_button != null) {
+            switch_porter_duff_button.setOnCheckedChangeListener((buttonView, isChecked) -> {
+                if (isChecked) {
+                    int color = getResources().getColor(R.color.design_default_color_primary);
+                    PorterDuffColorFilter filter = new PorterDuffColorFilter(color, PorterDuff.Mode.CLEAR);
+                    pcv_color_view_2.setPorterDuffColorFilter(filter, 0);
+                } else {
+                    pcv_color_view_2.setPorterDuffColorFilter(null, 0);
+                }
+            });
+
+            bt_change_mode.setOnClickListener(v -> {
+                PorterDuff.Mode[] porterColorModes = pcv_color_view_2.getPorterColorModes();
+                if (mCurrentPorterColorMode >= porterColorModes.length) {
+                    mCurrentPorterColorMode = 0;
+                }
+                PorterDuff.Mode mode = porterColorModes[mCurrentPorterColorMode];
+                PorterDuffColorFilter filter = new PorterDuffColorFilter(Color.BLUE, mode);
+                pcv_color_view_2.setPorterDuffColorFilter(filter, mCurrentPorterColorMode);
+                mCurrentPorterColorMode++;
+            });
+        }
+    }
+
+    private void initMatrixColorFilter() {
+        // 4 row * 5 column
+        matrixColorArray = new float[20];
+        if (seek_red != null && pcv_color_view_3 != null) {
+            seek_red.setOnSeekBarChangeListener(this);
+            seek_green.setOnSeekBarChangeListener(this);
+            seek_blue.setOnSeekBarChangeListener(this);
+            seek_alpha.setOnSeekBarChangeListener(this);
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        int id = seekBar.getId();
+        int progress = seekBar.getProgress();
+        switch (id) {
+            case R.id.seek_red:
+                colorRed = progress;
+                matrixColorArray[0] = colorRed;
+                matrixColorArray[1] = 0;
+                matrixColorArray[2] = 0;
+                matrixColorArray[3] = 0;
+                matrixColorArray[4] = offset;
+                break;
+            case R.id.seek_green:
+                colorGreen = progress;
+                matrixColorArray[5] = colorGreen;
+                matrixColorArray[6] = 0;
+                matrixColorArray[7] = 0;
+                matrixColorArray[8] = 0;
+                matrixColorArray[9] = offset;
+                break;
+            case R.id.seek_blue:
+                colorBlue = progress;
+                matrixColorArray[10] = colorBlue;
+                matrixColorArray[11] = 0;
+                matrixColorArray[12] = 0;
+                matrixColorArray[13] = 0;
+                matrixColorArray[14] = offset;
+                break;
+            case R.id.seek_alpha:
+                colorAlpha = progress;
+                matrixColorArray[15] = colorAlpha;
+                matrixColorArray[16] = colorAlpha;
+                matrixColorArray[17] = colorAlpha;
+                matrixColorArray[18] = colorAlpha;
+                matrixColorArray[19] = offset;
+                break;
+        }
+
+        ColorMatrixColorFilter colorMatrixColorFilter = new ColorMatrixColorFilter(matrixColorArray);
+        pcv_color_view_3.setMatrixColorFilter(colorMatrixColorFilter);
     }
 }
--- app/src/main/java/com/dan/learn/lab2/widget/view/PaintColorView.java
+++ app/src/main/java/com/dan/learn/lab2/widget/view/PaintColorView.java
@@ -6,7 +6,10 @@ import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorFilter;
+import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
 import android.graphics.RectF;
 import android.util.AttributeSet;
 import android.view.View;
@@ -23,18 +26,45 @@ import com.dan.learn.lab2.R;
  */
 public class PaintColorView extends View {
 
+    private PorterDuff.Mode[] modes = {
+            PorterDuff.Mode.CLEAR,
+            PorterDuff.Mode.SRC,
+            PorterDuff.Mode.DST,
+            PorterDuff.Mode.SRC_OVER,
+            PorterDuff.Mode.DST_OVER,
+            PorterDuff.Mode.SRC_IN,
+            PorterDuff.Mode.DST_IN,
+            PorterDuff.Mode.SRC_OUT,
+            PorterDuff.Mode.DST_OUT,
+            PorterDuff.Mode.SRC_ATOP,
+            PorterDuff.Mode.DST_ATOP,
+            PorterDuff.Mode.XOR,
+            PorterDuff.Mode.DARKEN,
+            PorterDuff.Mode.LIGHTEN,
+            PorterDuff.Mode.MULTIPLY,
+            PorterDuff.Mode.SCREEN
+    };
+    private String[] modeStr = {
+            "Clear", "SRC", "DST", "SRC_OVER", "DST_OVER", "SRC_IN",
+            "DST_IN", "SRC_OUT", "DST_OUT", "SRC_ATOP", "DST_ATOP", "XOR", "DARKEN",
+            "LIGHTEN", "MULTIPLY", "SCREEN"
+    };
+
     private static final int FILTER_MODE_LIGHT = 1;
     private static final int FILTER_PORTER_DUFF = 2;
     private static final int FILTER_COLOR_MATRIX = 3;
+
+    private PorterDuffColorFilter porterDuffColorFilter;
 
     private Paint mPaint;
 
     private RectF rectF = new RectF();
 
-    private int colorMul;
-    private int colorAdd;
-    private int filterMode = 1;
+    private RectF rectF2 = new RectF();
+
+    private int filterMode;
     private Bitmap bitmap;
+    private int porterDuffColorFilterIndex;
 
     public PaintColorView(Context context) {
         this(context, null);
@@ -54,11 +84,45 @@ public class PaintColorView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-        drawRoundRect(canvas);
         if (filterMode == FILTER_MODE_LIGHT) {
+            drawRoundRect(canvas);
             drawLightColorFilterRect(canvas);
             drawLightColorFilterBitmap(canvas);
+        } else if (filterMode == FILTER_PORTER_DUFF) {
+            drawPorterDuffColorFilter(canvas);
+        } else if (filterMode == FILTER_COLOR_MATRIX) {
+            drawColorMatrixFilter(canvas);
         }
+    }
+
+    private void drawPorterDuffColorFilter(Canvas canvas) {
+        mPaint.setTextSize(20);
+
+        mPaint.setColorFilter(null);
+        canvas.drawBitmap(bitmap, 10, 20, mPaint);
+        mPaint.setColorFilter(porterDuffColorFilter);
+        canvas.drawBitmap(bitmap, 100, 20, mPaint);
+
+        canvas.drawBitmap(bitmap, 220, 20, mPaint);
+        canvas.drawBitmap(bitmap, 290, 20, mPaint);
+
+        rectF.set(400, 20, 500, 120);
+        rectF2.set(460, 60, 560, 160);
+
+        mPaint.setColor(Color.RED);
+        canvas.drawRect(rectF, mPaint);
+        mPaint.setColor(Color.GREEN);
+        canvas.drawRect(rectF2, mPaint);
+
+        mPaint.setColorFilter(null);
+        mPaint.setColor(Color.CYAN);
+        canvas.drawText(modeStr[porterDuffColorFilterIndex], 10, 20, mPaint);
+    }
+
+    private void drawColorMatrixFilter(Canvas canvas) {
+        mPaint.setColor(Color.RED);
+        rectF.set(100, 20, 300, 320);
+        canvas.drawRect(rectF, mPaint);
     }
 
     private void drawRoundRect(Canvas canvas) {
@@ -74,9 +138,18 @@ public class PaintColorView extends View {
         invalidate();
     }
 
-    public void setLightColorFilterValue(int mul, int add) {
-        colorAdd = add;
-        colorMul = mul;
+    public void setPorterDuffColorFilter(PorterDuffColorFilter filter, int index) {
+        filterMode = FILTER_PORTER_DUFF;
+        porterDuffColorFilter = filter;
+        porterDuffColorFilterIndex = index;
+        invalidate();
+    }
+
+    public void setMatrixColorFilter(ColorMatrixColorFilter matrixColorFilter) {
+        filterMode = FILTER_COLOR_MATRIX;
+        if (mPaint != null) {
+            mPaint.setColorFilter(matrixColorFilter);
+        }
         invalidate();
     }
 
@@ -84,12 +157,13 @@ public class PaintColorView extends View {
         rectF.set(230, 10, 360, 300);
         canvas.drawRect(rectF, mPaint);
         mPaint.setColor(Color.parseColor("#326b6b"));
-        String text = "mul :" + colorMul + " add:" + colorAdd;
-        canvas.drawText(text, 200, 310, mPaint);
     }
 
     private void drawLightColorFilterBitmap(Canvas canvas) {
         canvas.drawBitmap(bitmap, 380, 10, mPaint);
     }
 
+    public PorterDuff.Mode[] getPorterColorModes() {
+        return modes;
+    }
 }
