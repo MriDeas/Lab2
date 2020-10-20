898ac71cc28154c62dd3d8a9876382a539231b82
dan <kaqilon@aliyun.com>
2020-10-20 22:49:55 +0800

    SimpleViewPager

--- app/src/main/java/com/dan/learn/lab2/ui/activity/ViewDemoActivity.java
+++ app/src/main/java/com/dan/learn/lab2/ui/activity/ViewDemoActivity.java
@@ -1,20 +1,25 @@
 package com.dan.learn.lab2.ui.activity;
 
 import com.dan.learn.lab2.R;
+import com.dan.learn.lab2.ui.base.BaseActivity;
 import com.dan.learn.lab2.ui.base.BaseFragment;
 import com.dan.learn.lab2.ui.base.BaseMultiFragmentActivity;
 import com.dan.learn.lab2.ui.fragment.ViewDemoFragment;
 
 import java.util.List;
 
-public class ViewDemoActivity extends BaseMultiFragmentActivity {
+public class ViewDemoActivity extends BaseActivity {
+//BaseMultiFragmentActivity
+//    @Override
+//    protected List<BaseFragment> getFragments(List<BaseFragment> fragments) {
+////        fragments.add(new ViewDemoFragment("Tab_Layout", R.layout.fragment_tab_layout));
+////        fragments.add(new ViewDemoFragment("Touch_View", R.layout.fragment_touch_layout));
+//        fragments.add(new ViewDemoFragment("View_Pager", R.layout.fragment_view_pager));
+//        return fragments;
+//    }
 
     @Override
-    protected List<BaseFragment> getFragments(List<BaseFragment> fragments) {
-        fragments.add(new ViewDemoFragment("Tab_Layout", R.layout.fragment_tab_layout));
-        fragments.add(new ViewDemoFragment("Touch_View", R.layout.fragment_touch_layout));
-        fragments.add(new ViewDemoFragment("View_Pager", R.layout.fragment_view_pager));
-        return fragments;
+    protected int getContentLayout() {
+        return R.layout.fragment_view_pager;
     }
-
 }
--- app/src/main/java/com/dan/learn/lab2/widget/view/SimpleViewPager.java
+++ app/src/main/java/com/dan/learn/lab2/widget/view/SimpleViewPager.java
@@ -5,15 +5,28 @@ import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
+import android.widget.OverScroller;
 
 public class SimpleViewPager extends ViewGroup {
 
     private VelocityTracker velocityTracker;
+    private float downX;
+    private final ViewConfiguration mViewConfiguration;
+    private boolean isScrolling;
+    private OverScroller mScroller;
+    private float maxXVelocity;
+    private float minXVelocity;
+    private int downScrollX;
 
     public SimpleViewPager(Context context, AttributeSet attrs) {
         super(context, attrs);
         velocityTracker = VelocityTracker.obtain();
+        mViewConfiguration = ViewConfiguration.get(context);
+        mScroller = new OverScroller(context);
+        maxXVelocity = mViewConfiguration.getScaledMaximumFlingVelocity();
+        minXVelocity = mViewConfiguration.getScaledMinimumFlingVelocity();
     }
 
     @Override
@@ -39,13 +52,82 @@ public class SimpleViewPager extends ViewGroup {
 
     @Override
     public boolean onInterceptTouchEvent(MotionEvent ev) {
-
-        return super.onInterceptTouchEvent(ev);
+        if (ev.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            velocityTracker.clear();
+        }
+        boolean result = false;
+        switch (ev.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downX = ev.getX();
+                downScrollX = getScrollX();
+                isScrolling = false;
+                break;
+            case MotionEvent.ACTION_MOVE:
+                float dX = downX - ev.getX();
+                System.out.println("打印 dX ------------ " + dX);
+                if (!isScrolling) {
+                    if (Math.abs(dX) > mViewConfiguration.getScaledPagingTouchSlop()) {
+                        isScrolling = true;
+                        getParent().requestDisallowInterceptTouchEvent(true);
+                        result = true;
+                    }
+                }
+                break;
+        }
+        return result;
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
+        if (event.getActionMasked() == MotionEvent.ACTION_DOWN) {
+            velocityTracker.clear();
+        }
+        velocityTracker.addMovement(event);
+        float x = event.getX();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                downX = x;
+                isScrolling = false;
+                downScrollX = getScrollX(); //scrollX 是View左边界滑动的距离，向左变大，向右变小. 有负值
+                break;
+            case MotionEvent.ACTION_MOVE:
+                velocityTracker.getXVelocity();
+                float dX = downX - x + downScrollX;
+                if (dX < 0) {
+                    dX = 0;
+                } else if (dX > getWidth()) {
+                    dX = getWidth();
+                }
+                scrollTo((int) dX, 0);
+                break;
+            case MotionEvent.ACTION_UP:
+                //计算速度
+                //判断速度 和 移动方向， 速度大于某常量时，开始向一边位移
+                velocityTracker.computeCurrentVelocity(1000, maxXVelocity);
+                float xV = velocityTracker.getXVelocity();
+                int scrollX = getScrollX();
+                int targetPage;
+                if (Math.abs(xV) < minXVelocity) {
+                    // 滑动距离大于View宽度一半时，页面翻新，否则到原页面
+                    targetPage = scrollX > getWidth() / 2 ? 1 : 0;
+                } else {
+                    // 速度 小于0，表示向左，否则向右
+                    targetPage = xV < 0 ? 1 : 0;
+                }
+                //距离根据页面判断
+                int scrollDistance = targetPage == 1 ? getWidth() - scrollX : -scrollX;
+                mScroller.startScroll(getScrollX(), 0, scrollDistance, 0);
+                postInvalidateOnAnimation(); //下一帧执行时（延迟）会调用invalidate()，然后执行 draw -> computeScroll
+                break;
+        }
+        return true;
+    }
 
-        return super.onTouchEvent(event);
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            scrollTo(mScroller.getCurrX(), 0);
+            postInvalidateOnAnimation();
+        }
     }
 }
