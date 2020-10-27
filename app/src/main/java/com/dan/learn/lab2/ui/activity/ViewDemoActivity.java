a1b7b0c43351b90d0ba04d47b54dfcdd1b2ce603
dan <kaqilon@aliyun.com>
2020-10-27 23:02:47 +0800

    vector

--- app/src/main/java/com/dan/learn/lab2/ui/activity/ViewDemoActivity.java
+++ app/src/main/java/com/dan/learn/lab2/ui/activity/ViewDemoActivity.java
@@ -1,5 +1,14 @@
 package com.dan.learn.lab2.ui.activity;
 
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
+import android.animation.ValueAnimator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.view.animation.ScaleAnimation;
+import android.view.animation.TranslateAnimation;
+
 import com.dan.learn.lab2.R;
 import com.dan.learn.lab2.ui.base.BaseFragment;
 import com.dan.learn.lab2.ui.base.BaseMultiFragmentActivity;
@@ -14,6 +23,7 @@ public class ViewDemoActivity extends BaseMultiFragmentActivity {
 //        fragments.add(new ViewDemoFragment("Tab_Layout", R.layout.fragment_tab_layout));
 //        fragments.add(new ViewDemoFragment("Touch_View", R.layout.fragment_touch_layout));
         fragments.add(new ViewDemoFragment("View_Pager", R.layout.fragment_view_pager));
+
         return fragments;
     }
 
@@ -21,4 +31,14 @@ public class ViewDemoActivity extends BaseMultiFragmentActivity {
 //    protected int getContentLayout() {
 //        return R.layout.fragment_view_pager;
 //    }
+
+    private void animation() {
+        Animation a = new ScaleAnimation(1, 1, 1.5f, 1.5f);
+        Animation b = new TranslateAnimation(0, 0, 100, 100);
+        Animation c = new AlphaAnimation(1, 0.86f);
+        Animation d = new RotateAnimation(0, 90);
+        ObjectAnimator f = ObjectAnimator.ofFloat(null, "title", 1, 2, 3, 4, 5, 6, 7);
+        ValueAnimator g = ValueAnimator.ofFloat(100);
+        PropertyValuesHolder p = PropertyValuesHolder.ofFloat("translationX",300f);
+    }
 }
