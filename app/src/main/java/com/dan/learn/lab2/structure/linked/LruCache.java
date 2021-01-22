13179c22b64e9db4ce37a235d8e07523af98ae15
kaqilon <kaqilon@aliyun.com>
2021-01-22 15:00:14 +0800

    1. 添加数据结构支持

---
+++ app/src/main/java/com/dan/learn/lab2/structure/linked/LruCache.java
@@ -0,0 +1,13 @@
+package com.dan.learn.lab2.structure.linked;
+
+/**
+ * Created by: dan
+ * Created time: 2021/1/21
+ * Description:
+ * Modify time:
+ */
+public class LruCache {
+
+
+
+}
---
+++ app/src/main/java/com/dan/learn/lab2/structure/stack/LinkedListStack.java
@@ -0,0 +1,11 @@
+package com.dan.learn.lab2.structure.stack;
+
+/**
+ * Created by: dan
+ * Created time: 2021/1/21
+ * Description:
+ * Modify time:
+ */
+public class LinkedListStack {
+
+}
